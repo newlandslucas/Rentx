5f1c3084f8b5d4820c949f6e36539e4521104790
Lucas Newlands <58925749+newlandslucas@users.noreply.github.com>
2021-08-02 16:20:47 -0300

    Global Theme | external fonts

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container, Title } from './styles';
+
+export function Home() {
+    return(
+        <Container>
+            <Title>
+                HOME
+            </Title>
+        </Container>
+    )
+}
